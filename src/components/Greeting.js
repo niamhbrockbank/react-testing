edc0fb09e8130173a451748db7978e8f1362cc0c
Niamh Brockbank <niamh.brockbank@imgarena.com>
2025-06-17 12:45:49 +0100

    Add user interaction test

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,7 +1,16 @@
+import { useState } from "react"
+
 const Greeting = () => {
+    const [changedText, setChangedText] = useState(false)
+
+    function handleChangeText(){
+        setChangedText(prev => !prev)
+    }
+
     return <div>
         <h2>Hello world</h2>
-        <p>Good to see you</p>
+        {!changedText ? <p>Good to see you</p> : <p>Changed!</p>}
+        <button onClick={handleChangeText}>Change Text!</button>
     </div>
 }
 
--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,14 +1,35 @@
 import Greeting from "./Greeting"
 import { render, screen } from "@testing-library/react"
+import userEvent from "@testing-library/user-event"
 
-test('hello world shows on screen as text', () => {
-    // Arrange
-    render(<Greeting />)
+describe('Greeting component', () => {
+    test('hello world shows on screen as text', () => {
+        // Arrange
+        render(<Greeting />)
 
-    // Act
-    // ...nothing
+        // Act
+        // ...nothing
 
-    // Assert
-    const helloWorldElement = screen.getByText('Hello world', { exact : false})
-    expect(helloWorldElement).toBeInTheDocument()
+        // Assert
+        const helloWorldElement = screen.getByText('Hello world', { exact : false})
+        expect(helloWorldElement).toBeInTheDocument()
+    })
+
+    test('renders good to se you before button is clicked', () => {
+        render(<Greeting />)
+
+        const goodToSeeYouElement = screen.getByText('Good to see you')
+        expect(goodToSeeYouElement).toBeInTheDocument()
+    })
+
+    test('renders changed! after button is clicked', () => {
+        render(<Greeting />)
+
+        const changeTextButton = screen.getByRole('button')
+        userEvent.click(changeTextButton)
+
+        const changedElement = screen.getByText('Changed', {exact : false})
+        expect(changedElement).toBeInTheDocument()
+    })
+
 })
